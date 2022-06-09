f565f636909a4c986383dc60c4b75686a95e8bf3
arman <mr.kazemi.ar@gmail.com>
2022-06-10 00:06:47 +0430

    feat override method save in model Member in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class Member(AbstractUser):
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'username'
 
-
-
-
+    def save(self, *args, **kwargs):
+        if self.role == 'staff':
+            self.is_staff = True
+        super(Member, self).save(*args, **kwargs)
