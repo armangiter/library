cea86843248c13f08099baa06803aa5d8d81170f
arman <mr.kazemi.ar@gmail.com>
2022-06-09 22:47:56 +0430

    initial Book and Barrow Model in librarian app

--- librarian/models.py
+++ librarian/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import BaseModel
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(BaseModel):
+    name = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=50, verbose_name='ISBN', help_text='International Standard Book Number')
+    inventory = models.IntegerField(default=0)
+
+
+class Barrow(BaseModel):
+    barrow_date = models.DateField(default=timezone.now)
+    return_date = models.DateField()
