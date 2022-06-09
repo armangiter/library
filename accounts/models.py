207f0f393cc7c76973be9e09dd812273b43074a2
arman <mr.kazemi.ar@gmail.com>
2022-06-09 22:47:10 +0430

    initial Custom User model in accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Member(AbstractUser):
+    role = models.CharField(max_length=10, choices=(('staff', 'is staff'), ('member',  'is member')))
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+
+
+
+
