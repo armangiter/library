8597e15599849f8338aad41022a17cf3a33f47a4
arman <mr.kazemi.ar@gmail.com>
2022-06-10 19:20:44 +0430

    override method save for model Barrow: to check inventory value

--- librarian/models.py
+++ librarian/models.py
@@ -30,3 +30,9 @@ class Barrow(BaseModel):
     barrow_date = models.DateTimeField(default=timezone.now)
     barrow_time = models.DurationField(default=timezone.timedelta(days=7))
     return_date = models.DateTimeField()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None
+             ):
+        if self.book.subtract_inventory():
+            self.return_date = self.barrow_date + self.barrow_time
+            super(Barrow, self).save(force_insert=False, force_update=False, using=None, update_fields=None)
