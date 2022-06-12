ebe7ac5f8d9050a97446658f14cd5305c0f44f2f
arman <mr.kazemi.ar@gmail.com>
2022-06-12 15:36:36 +0430

    feat barrow serializer

--- librarian/serializers.py
+++ librarian/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from librarian.models import Book
+from librarian.models import Book, Barrow
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'name', 'inventory', 'publisher', 'author')
+
+
+class BarrowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Barrow
+        fields = ('id', 'book', 'member', 'barrow_date', 'barrow_time', 'return_date')
